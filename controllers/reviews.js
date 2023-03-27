e7e5a0c8fa8048f17e8025920c87e978a8543716
Vadim Alexeev <valexeev30@gmail.com>
2023-03-27 15:16:45 +0100

    Remove stray console logs

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,14 +2,13 @@ const {fetchSingleReview} = require("../models/reviews")
 
 exports.getReviewByID = (req, res, next)=>{
     const { review_id } = req.params;
-    console.log(review_id)
+    
     fetchSingleReview(review_id).then((review)=>{
         res.status(200).send({
             "review": review
         })
     })
     .catch((err)=>{
-        console.log("ERROR: ", err);
         next(err)
     })
 }

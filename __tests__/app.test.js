39ce15ce8fe1eca9a3d88e35c8afef207fd693cb
Vadim Alexeev <valexeev30@gmail.com>
2023-03-31 11:49:50 +0100

    Test for correct object formatting

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -308,9 +308,17 @@ describe("GET: /api/users", ()=>{
             .expect(200)
             .then(({body})=>{
                 const { users } = body;
-
+                
                 expect(users).toBeInstanceOf(Array);
                 expect(users.length).toBe(4);
+
+                users.forEach((user)=>{
+                    expect(user).toMatchObject({
+                        username: expect.any(String),
+                        name: expect.any(String),
+                        avatar_url: expect.any(String)
+                    })
+                })
             })
     })
 })
